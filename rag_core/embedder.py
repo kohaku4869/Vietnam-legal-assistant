cd5f9c6aaf75daf9a79216519fb5ec94ff47cf39
kohaku4869 <83518015+kohaku4869@users.noreply.github.com>
2025-07-14 01:57:37 +0700

    small change

--- rag_core/embedder.py
+++ rag_core/embedder.py
@@ -1,6 +1,7 @@
 from langchain_huggingface import HuggingFaceEmbeddings
 import numpy as np
 
+#Embedder
 class Embedder:
     def __init__(self, model_name: str = "intfloat/multilingual-e5-large"):
         self.embedding = HuggingFaceEmbeddings(model_name=model_name)
